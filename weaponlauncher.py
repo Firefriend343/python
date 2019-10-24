573d672f642b74138c396aaadd663eee8fcdebdc
Firefriend343 <wheeler.hawk@gmail.com>
2019-10-24 11:36:22 -0700

    Create weaponlauncher.py

---
+++ weaponlauncher.py
@@ -0,0 +1,5 @@
+noun = raw_input("Choose target of destruction: ")
+verb = raw_input("Give command to fire")
+
+print("You have targeted" + noun)
+print("You have given the order to " + verb + "." + noun + "was destroyed.")
