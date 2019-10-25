4c831553ddd9370c1bf09c31dbfed3da32f43c91
Firefriend343 <wheeler.hawk@gmail.com>
2019-10-24 19:53:25 -0700

    Update and rename weaponlauncher.html to weaponlauncher.py

--- weaponlauncher.py
+++ weaponlauncher.py
@@ -1,12 +1,7 @@
-<html>
-  <script>
   #We are making a test program
   noun = raw_input("Choose target of destruction: ")
   verb = raw_input("Give command to fire")
 
   print("You have targeted" + noun)
   print("You have given the order to " + verb + "." + noun + "was destroyed.")
-  </script>
-  <body>
-  </body>
-</html>
+  
